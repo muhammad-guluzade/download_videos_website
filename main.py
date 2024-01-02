64f818f4501ecb161b823628cfb7c4a5bce0ca6b
malai320 <127290622+muhammad-guluzade@users.noreply.github.com>
2024-01-03 01:35:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,43 @@
+import os
+import pytube
 from pytube import YouTube
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
+FILENAME = ""
 
 @app.route("/", methods=["GET", "POST"])
 def download_page():
-    global session
+    global session, FILENAME
     if request.method == "POST":
-        video = YouTube(request.form['link'])
-        stream = video.streams.filter(only_audio=True).first()
-        stream.download(filename=f"static/video.mp3")
+        try:
+            video = YouTube(request.form['link'])
+        except pytube.exceptions.RegexMatchError:
+            return render_template("download.html",
+                                   show_download=False,
+                                   error_message="You entered invalid link")
+        
+        try:
+            stream = video.streams.filter(only_audio=True).first()
+        except pytube.exceptions.VideoUnavailable:
+            return render_template("download.html",
+                                   show_download=False,
+                                   error_message="You entered invalid link")
+        
+        FILENAME = f"static/{video.title}.mp3"
+        stream.download(filename=FILENAME)
         return render_template("download.html",
-                               show_download=True)
+                               show_download=True,
+                               filename=FILENAME,
+                               title=FILENAME.strip(".mp3").strip("static/"))
     return render_template("download.html",
                            show_download=False)
+
+
+@app.route("/delete")
+def delete_file():
+    os.remove(FILENAME)
+    return redirect("/")
 
 
 if __name__ == "__main__":
