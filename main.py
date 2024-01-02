740c62e90cf8f48de406b9dfea53664d9b07ab51
malai320 <127290622+muhammad-guluzade@users.noreply.github.com>
2024-01-03 00:44:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def download_page():
+    global session
     if request.method == "POST":
         video = YouTube(request.form['link'])
         stream = video.streams.filter(only_audio=True).first()
-        stream.download(filename=f"{video.title}.mp3")
-        return redirect("/")
-    return render_template("download.html")
+        stream.download(filename=f"static/video.mp3")
+        return render_template("download.html",
+                               show_download=True)
+    return render_template("download.html",
+                           show_download=False)
 
 
 if __name__ == "__main__":
