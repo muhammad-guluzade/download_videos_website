cca1282072f53fb3d2e2add947dfd68f1e187586
malai320 <127290622+muhammad-guluzade@users.noreply.github.com>
2024-01-03 16:54:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,30 +8,35 @@ FILENAME = ""
 
 @app.route("/", methods=["GET", "POST"])
 def download_page():
-    global session, FILENAME
-    if request.method == "POST":
-        try:
-            video = YouTube(request.form['link'])
-        except pytube.exceptions.RegexMatchError:
+    global FILENAME
+    try:
+        if request.method == "POST":
+            try:
+                video = YouTube(request.form['link'])
+            except pytube.exceptions.RegexMatchError:
+                return render_template("download.html",
+                                       show_download=False,
+                                       error_message="You entered invalid link")
+    
+            try:
+                stream = video.streams.filter(only_audio=True).first()
+            except pytube.exceptions.VideoUnavailable:
+                return render_template("download.html",
+                                       show_download=False,
+                                       error_message="You entered invalid link")
+    
+            FILENAME = f"static/{video.title}.mp3"
+            stream.download(filename=FILENAME)
             return render_template("download.html",
-                                   show_download=False,
-                                   error_message="You entered invalid link")
-
-        try:
-            stream = video.streams.filter(only_audio=True).first()
-        except pytube.exceptions.VideoUnavailable:
-            return render_template("download.html",
-                                   show_download=False,
-                                   error_message="You entered invalid link")
-
-        FILENAME = f"static/{video.title}.mp3"
-        stream.download(filename=FILENAME)
+                                   show_download=True,
+                                   filename=FILENAME,
+                                   title=FILENAME.strip(".mp3").strip("static/"))
         return render_template("download.html",
-                               show_download=True,
-                               filename=FILENAME,
-                               title=FILENAME.strip(".mp3").strip("static/"))
-    return render_template("download.html",
-                           show_download=False)
+                               show_download=False)
+    except Exception as e:
+        return render_template("download.html",
+                                       show_download=False,
+                                       error_message=f"{e}")
 
 
 @app.route("/delete")
