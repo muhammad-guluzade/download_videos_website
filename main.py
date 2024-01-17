c1eb222244cc3819b63063ee1e55538083967b06
malai320 <127290622+muhammad-guluzade@users.noreply.github.com>
2024-01-17 07:24:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ FILENAME = ""
 def download_page():
     global FILENAME
     try:
+        
         if request.method == "POST":
             try:
                 video = YouTube(request.form['link'])
@@ -20,17 +21,33 @@ def download_page():
     
             try:
                 stream = video.streams.filter(only_audio=True).first()
+                stream_mp4_720 = video.streams.filter(mime_type="video/mp4", res="720p").first()
+                stream_mp4_360 = video.streams.filter(mime_type="video/mp4", res="360p").first()
+                
             except pytube.exceptions.VideoUnavailable:
                 return render_template("download.html",
                                        show_download=False,
                                        error_message="You entered invalid link")
             title = video.title.replace('/', '|').replace('\\', '|')
             FILENAME = f"static/{title}.mp3"
+            FILENAME_360 = f"static/{title}360.mp4"
+            FILENAME_720 = f"static/{title}720.mp4"
             stream.download(filename=FILENAME)
+
+            if stream_mp4_360:
+                stream_mp4_360.download(filename=FILENAME_360)
+
+            if stream_mp4_720:
+                stream_mp4_720.download(filename=FILENAME_720)
+
             return render_template("download.html",
                                    show_download=True,
                                    filename=FILENAME,
-                                   title=title)
+                                   title=title,
+                                   video_360=stream_mp4_360,
+                                   video_720=stream_mp4_720,
+                                   filename_360=FILENAME_360,
+                                   filename_720=FILENAME_720)
         return render_template("download.html",
                                show_download=False)
     except Exception as e:
